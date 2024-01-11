eeb9bc7607aa37fb0a7e1768b9dd83f8aa2c0b9c
oh young seok <oyg9731@naver.com>
2024-01-11 10:57:22 +0900

    #9. Todo 체크완료 기능

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -1,18 +1,16 @@
 import React, {useState} from "react";
-import {MdAdd, MdDelete, MdDone} from "react-icons/md";
-
 import './scss/TodoTemplate.scss';
 import TodoHeader from "./TodoHeader";
 import TodoInput from "./TodoInput";
 import TodoMain from "./TodoMain";
 
+
 const TodoTemplate = () => {
 
     /*
-         리액트는 부모컴포넌트에서 자식컴포넌트로의 데이터이동이 반대보다 쉽기 때문에
-         할 일 데이터는 상위부모컴포넌트에서 처리하는것이 좋다
+        리액트는 부모컴포넌트에서 자식컴포넌트로의 데이터이동이 반대보다 쉽기 때문에
+        할 일 데이터는 상위부모컴포넌트에서 처리하는것이 좋다
      */
-
     const [todoList, setTodoList] = useState([
         {
             id: 1,
@@ -33,46 +31,65 @@ const TodoTemplate = () => {
     ]);
 
     // 데이터 상향식 전달을 위해 부모가 자식에게 함수를 하나 전달
-    const addTodo = (todoText)=>{
+    const addTodo = (todoText) => {
         // console.log('할 일 등록 함수를 todotemplate에서 실행!');
-        console.log('textText: ', todoText)
+        console.log('todoText: ', todoText);
 
         const makeNewId = () => {
-            return todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1;
-        }
+            if (todoList.length === 0) return 1;
+            return todoList[todoList.length - 1].id + 1;
+        };
 
-        const newTodo  ={
+        const newTodo = {
             id: makeNewId(),
             title: todoText,
             done: false
-        }
-        // todoList.push(newTodo)
+        };
+
+        // todoList.push(newTodo);
+
         /*
             상태변수의 변경은 오로지 setter를 통해서만 가능
             상태값이 변경감지가 되면 리액트는 렌더링을 다시 시작합니다.
-            다만 상태변수가 const 형태로 불변성을 띄기 때문에
-            기존의 상태값을 바꾸는것은 불가능하고
+            다만 상태변수가 const형태로 불변성을 띄기 때문에
+            기존의 상태값을 바꾸는 것은 불가능하고
             새로운 상태를 만들어서 바꿔야 합니다.
          */
 
-        setTodoList([...todoList,newTodo]);
+        setTodoList(prevState => [...prevState, newTodo]);
     };
+
 
     // 할 일 삭제 처리 함수
     const removeTodo = id => {
-        console.log('id: ', id)
-
-
-
+        // console.log('id:', id);
         setTodoList(todoList.filter(todo => todo.id !== id));
     };
 
+    // 할 일 체크 처리 함수
+    const checkTodo = id => {
+        // console.log('check id: ', id);
+
+        // const copyTodoList = [...todoList];
+        //
+        // const foundTodo = copyTodoList.find(todo => todo.id === id);
+        // foundTodo.done = !foundTodo.done;
+        //
+        // setTodoList(copyTodoList);
+
+        setTodoList(todoList.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo));
+
+    };
+
+
+    // 체크가 안된 할일 개수 카운트하기
+    const countRestTodo = todoList.filter(todo => !todo.done).length;
 
     return (
         <div className='TodoTemplate'>
-            <TodoHeader/>
-            <TodoMain todoList={todoList} onRemove={removeTodo}/>
-            <TodoInput onAdd={addTodo}/>
+            <TodoHeader count={countRestTodo}/>
+            <TodoMain todoList={todoList} onRemove={removeTodo} onCheck={checkTodo} />
+            <TodoInput onAdd={addTodo} />
         </div>
     );
 };
