586a633c7618831d017b38754d4b281abfdebc8b
oh young seok <oyg9731@naver.com>
2024-01-19 11:28:41 +0900

    #11. 리액트 라우터 돔으로 회원가입 페이지 전환

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoTemplate from "./component/TodoTemplate";
+import TodoTemplate from "./component/todo/TodoTemplate";
+import Header from "./component/layout/Header";
+import Footer from "./component/layout/Footer";
+import Join from "./component/user/Join";
+import {Route, Routes} from "react-router-dom";
+import todoTemplate from "./component/todo/TodoTemplate";
 
 function App() {
     return (
         <>
-            <TodoTemplate/>
+            <Header/>
+
+            <Routes>
+                <Route path='/' element={<TodoTemplate/>}/>
+                <Route path='/join' element={<Join/>}/>
+            </Routes>
+            <Footer/>
         </>
     );
 }
